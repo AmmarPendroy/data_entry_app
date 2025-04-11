ef2e63017d2faa18e39c4aa8fe31656c7645418e
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-11 11:43:15 +0300

    Update style.py

--- style.py
+++ style.py
@@ -1,51 +1,72 @@
 import streamlit as st
 
 def set_page_config():
+    # Setting the page title and layout
     st.set_page_config(page_title="Data Entry App", layout="centered")
 
-def apply_custom_styles(theme="light"):
-    if theme == "dark":
-        st.markdown(
-            """
+def apply_custom_styles(theme_choice):
+    if theme_choice == "light":
+        st.markdown("""
             <style>
-            body {
-                background-color: #0e1117;
-                color: #fafafa;
-            }
-            .stButton>button {
-                background-color: #1f77b4;
-                color: white;
-            }
+                body {
+                    background-color: white;
+                    color: black;
+                }
+                .stButton>button {
+                    background-color: #4CAF50;
+                    color: white;
+                }
+                .stTextInput input {
+                    background-color: white;
+                    color: black;
+                }
+                .stTextArea textarea {
+                    background-color: white;
+                    color: black;
+                }
             </style>
-            """,
-            unsafe_allow_html=True,
-        )
-    elif theme == "blue":
-        st.markdown(
-            """
+        """, unsafe_allow_html=True)
+    
+    elif theme_choice == "dark":
+        st.markdown("""
             <style>
-            body {
-                background-color: #e6f0ff;
-                color: #000000;
-            }
-            .stButton>button {
-                background-color: #0052cc;
-                color: white;
-            }
+                body {
+                    background-color: #2b2b2b;
+                    color: white;
+                }
+                .stButton>button {
+                    background-color: #0066cc;
+                    color: white;
+                }
+                .stTextInput input {
+                    background-color: #3c3c3c;
+                    color: white;
+                }
+                .stTextArea textarea {
+                    background-color: #3c3c3c;
+                    color: white;
+                }
             </style>
-            """,
-            unsafe_allow_html=True,
-        )
-    else:
-        # light
-        st.markdown(
-            """
+        """, unsafe_allow_html=True)
+
+    elif theme_choice == "blue":
+        st.markdown("""
             <style>
-            .stButton>button {
-                background-color: #4CAF50;
-                color: white;
-            }
+                body {
+                    background-color: #e0f7fa;
+                    color: #006064;
+                }
+                .stButton>button {
+                    background-color: #00838f;
+                    color: white;
+                }
+                .stTextInput input {
+                    background-color: #b2ebf2;
+                    color: #006064;
+                }
+                .stTextArea textarea {
+                    background-color: #b2ebf2;
+                    color: #006064;
+                }
             </style>
-            """,
-            unsafe_allow_html=True,
-        )
+        """, unsafe_allow_html=True)
